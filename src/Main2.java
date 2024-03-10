2b6067baf17d651ba85eff5dd1357f98c9926cde
Антон <anton_dolgopolov@mail.ru>
2024-03-11 00:54:55 +0500

    Calculator 2

--- src/Main2.java
+++ src/Main2.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class Main2 {
-    private static int RomanToArabic(String roman) { //Задаем эквивалент римской системе счисления согласно арабской если не соответствует то возвращаем значение переменной RomanToArabic -1
 
-        return -1;
-    }
 
     public static String calc(String input) throws Exception {             // После ввода строки строки определяем положение операндов поиском по строке
         int cnt = 0;                      // и так же устанавливаем значение переменной cnt (количество операндов)
@@ -27,17 +24,15 @@ public class Main2 {
         String rightArg = input.substring(operIndex + 1).trim();  // относительно операнда
 
         boolean isRome;
-        if (RomanToArabic(leftArg) > -1 && RomanToArabic(rightArg) > -1) {  //проверяем тип чисел римские или арабские,
-            isRome = true;
-        } else if (leftArg.matches("[\\d]{1,2}") && rightArg.matches("[\\d]{1,2}")) { //проверяем соответствует ли регулярному выражению левый и правый аргумент согласно маске и так же в одной системе счисления
+        if (leftArg.matches("[\\d]{1,2}") && rightArg.matches("[\\d]{1,2}")) { //проверяем соответствует ли регулярному выражению левый и правый аргумент согласно маске и так же в одной системе счисления
             isRome = false;
         } else {
             throw new Exception("Слева и справа должны быть или арабские или римские цифры одновременно в диапозоне от 1 до 10");
         }
 
         String oper = input.substring(operIndex, operIndex + 1); // Определяем тип переменной
-        int leftNum = isRome ? RomanToArabic(leftArg) : Integer.parseInt(leftArg);
-        int rightNum = isRome ? RomanToArabic(rightArg) : Integer.parseInt(rightArg);
+        int leftNum =  Integer.parseInt(leftArg);
+        int rightNum = Integer.parseInt(rightArg);
         if (leftNum < 1 || leftNum > 10 || rightNum < 1 || rightNum > 10) { //проверяем диапазон вводимых аргументов слева и справа должны быть от 1 до 10
             throw new Exception("Числа нужно вводить в диапазоне от 1 до 10");
         }
@@ -58,12 +53,8 @@ public class Main2 {
                 break;
         }
 
-        if (isRome && result <= 0) { //проверяем если переменная isRome= true и результат < 0 то выполняем условие
-            throw new Exception("В римской системе счисления числа должны быть больше >= 1");
-        }
-         else {
-            return "Ответ (В арабской системе счисления):" + result; //выводим результат в арабской системе счасления
-        }
+         return "Ответ (В арабской системе счисления):" + result; //выводим результат в арабской системе счасления
+
     }
 
     public static void main(String[] args) {
